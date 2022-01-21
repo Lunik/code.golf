ce3bb476b76e65dc66745c20db29b312067bba58
Lunik <lunik@tiwabbit.fr>
2022-01-22 00:01:03 +0100

    fix scores
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

--- solutions/abundant_numbers/__init__.py
+++ solutions/abundant_numbers/__init__.py
@@ -2,7 +2,7 @@ import os
 import cProfile
 import pstats
 
-# Score : 79 bytes, 79 chars
+# Score : 77 bytes, 77 chars
 def main():
   for i in range(1,201):
     if sum([j for j in range(1,i) if i%j==0])>i:print(i)
